7dd50fdaeab7a1755ba376fad3f2d0fb3b37ded8
Micsupreeme <micsupreemedev@gmail.com>
2021-03-08 17:47:11 +0000

    Init Commit

---
+++ CATS/PagedWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CATS
+{
+    /// <summary>
+    /// Interaction logic for PagedWindow.xaml
+    /// </summary>
+    public partial class PagedWindow : Window
+    {
+        int currentPageNumber = 0; //Page 0 is the first page
+
+        public PagedWindow()
+        {
+            InitializeComponent();
+            navigatePage(currentPageNumber); //Initialise the frame by setting it to show the first page
+        }
+
+        /*
+         * Navigates to the specified page index
+         * and adjusts the UI to match the specified page
+         */
+        private void navigatePage(int pageNumber)
+        {
+            switch (pageNumber)
+            {
+                case 0:
+                    mainFrame.Content = new TitleLevelPage();
+                    prevBtn.Visibility = Visibility.Hidden; //Cannot go back from the first page
+                    nextBtn.Visibility = Visibility.Visible;
+                    currentPageNumber = 0;
+                    break;
+                case 1:
+                    mainFrame.Content = new WeightDatePage();
+                    prevBtn.Visibility = Visibility.Visible;
+                    nextBtn.Visibility = Visibility.Visible;
+                    currentPageNumber = 1;
+                    break;
+                case 2:
+                    mainFrame.Content = new StaffSubPage();
+                    prevBtn.Visibility = Visibility.Visible;
+                    nextBtn.Visibility = Visibility.Hidden; //Cannot go forward from the last page
+                    currentPageNumber = 2;
+                    break;
+                default:
+                    Console.Error.WriteLine("ERROR: Invalid page specified");
+                    break;
+            }
+        }
+
+        private void prevBtn_Click(object sender, RoutedEventArgs e)
+        {
+            navigatePage(--currentPageNumber); //Go to the previous page
+        }
+
+        private void nextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            navigatePage(++currentPageNumber); //Go to the next page
+        }
+    }
+}
---
+++ CATS/TitleLevelPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CATS
+{
+    /// <summary>
+    /// Interaction logic for TitleLevelPage.xaml
+    /// </summary>
+    public partial class TitleLevelPage : Page
+    {
+        public TitleLevelPage()
+        {
+            InitializeComponent();
+        }
+
+        private void setSubState(int stateNumber)
+        {
+            try {
+                if (stateNumber == 0) {
+                    //Submission state
+                    asmtNoXCmb.SelectedIndex = 0;
+                    asmtNoYCmb.SelectedIndex = 1;
+                    asmtNoXCmb.IsEnabled = true;
+                    asmtNoYCmb.IsEnabled = true;
+                } else {
+                    //Resubmission state
+                    asmtNoXCmb.SelectedIndex = 0;
+                    asmtNoYCmb.SelectedIndex = 0;
+                    asmtNoXCmb.IsEnabled = false;
+                    asmtNoYCmb.IsEnabled = false; //Resubs are always "1 of 1"
+                }
+            } catch(NullReferenceException nrEx) {
+                Console.Error.WriteLine("ERROR: Attempted to access uninstantiated control");
+            }
+        }
+
+        private void subResubCmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (subResubCmb.SelectedValue.Equals("Submission")) {
+                setSubState(0);
+            } else {
+                setSubState(1);
+            }
+        }
+    }
+}
